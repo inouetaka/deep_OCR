ceeeb6a3d323453f1ca42eb9c350f96ba8406220
inouetaka <webroot63@gmail.com>
2019-11-19 15:06:08 +0900

    似たような処理は行をまとめた

--- tools/resize/image_resize.py
+++ tools/resize/image_resize.py
@@ -20,19 +20,15 @@ def gen_resize_img(path, opt):
     r_h, r_w, r_c = resize_img.shape
     if r_h <= opt.resize_height:
         H_PAD = opt.resize_height - r_h
-        b = np.full((H_PAD, r_w), int(mu_b))
-        g = np.full((H_PAD, r_w), int(mu_g))
-        r = np.full((H_PAD, r_w), int(mu_r))
+        b, g, r = np.full((H_PAD, r_w), int(mu_b)), np.full((H_PAD, r_w), int(mu_g)), np.full((H_PAD, r_w), int(mu_r))
         bgr = np.stack([b, g, r], 2)
         new_image = np.concatenate([resize_img, bgr], 0)
         print('before:{} => middle:{} => after:{}'.format(img.shape, resize_img.shape, new_image.shape))
     elif r_h > opt.resize_height:
         w_resize_img = cv2.resize(resize_img, (int(opt.resize_height * w / h), opt.resize_height))
-        r_h, r_w, r_c = w_resize_img.shape
+        r_h, r_w, _ = w_resize_img.shape
         W_PAD = opt.resize_width - r_w
-        b = np.full((r_h, W_PAD), int(mu_b))
-        g = np.full((r_h, W_PAD), int(mu_g))
-        r = np.full((r_h, W_PAD), int(mu_r))
+        b, g, r = np.full((r_h, W_PAD), int(mu_b)), np.full((r_h, W_PAD), int(mu_g)), np.full((r_h, W_PAD), int(mu_r))
         bgr = np.stack([b, g, r], 2)
         new_image = np.concatenate([w_resize_img, bgr], 1)
         print('before:{} => middle:{} => after:{}'.format(img.shape, resize_img.shape, new_image.shape))
@@ -53,8 +49,8 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_path', required=True, help='path to input image')
     parser.add_argument('--output_path', required=True, help='path to output image')
-    parser.add_argument('--resize_width', default=100, help='width you want to resize')
-    parser.add_argument('--resize_height', default=32, help='height you want to resize')
+    parser.add_argument('--resize_width', default=100, type=int, help='width you want to resize')
+    parser.add_argument('--resize_height', default=32, type=int, help='height you want to resize')
 
     opt = parser.parse_args()
 
---
+++ tools/resize/kaku.py
@@ -0,0 +1,7 @@
+import cv2
+
+path = './test_data/6_train_03319.png'
+
+img = cv2.imread(path)
+
+print(img)
