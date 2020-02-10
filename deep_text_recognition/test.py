d15aac354aca5d7522c322d5186aa4b94e8ec0a7
inouetaka <webroot63@gmail.com>
2020-02-10 17:23:30 +0900

    fix leventhtein

--- deep_text_recognition/test.py
+++ deep_text_recognition/test.py
@@ -134,12 +134,13 @@ def validation(model, criterion, evaluation_loader, converter, opt):
             edit = edit_distance(pred, gt)
             max_len = max(len(pred), len(gt))
             sum_ed += edit / max_len
+            levenshtein += edit / max_len
             #print("sum_ed", sum_ed)
 
 
         [print(f'predict: {p} | label: {l}\n') for p, l in zip(preds_str[:5], labels[:5])]
 
-        levenshtein += sum_ed / len(preds_str)
+        #levenshtein += sum_ed / len(preds_str)
         print("バッチごとのleven: ", round((sum_ed / len(preds_str)) * 100.0, 3), "%")
         print("sum_ed", sum_ed)
         print("pred_str", len(preds_str))
