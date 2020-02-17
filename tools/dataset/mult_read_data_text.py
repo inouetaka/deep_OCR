c9d7030665628e7f27c62deb228485cc323df698
inouetaka <webroot63@gmail.com>
2020-02-17 13:32:20 +0900

    print log

--- tools/dataset/mult_read_data_text.py
+++ tools/dataset/mult_read_data_text.py
@@ -19,21 +19,21 @@ def main(input_path, output_path, split_per, data_split):
             if 'count' in data.attrs:
                 count = data.attrs['count']
                 for i in range(count):
-                    print(f'instance {i}')
-                    print('character bounding boxes (2 x 4 x N):')
+                    #print(f'instance {i}')
+                    #print('character bounding boxes (2 x 4 x N):')
                     # print(d.attrs[f'charBB{i}'])
-                    print('text:')
-                    print(''.join([x.decode('utf8') for x in d.attrs[f'txt{i}']]))
+                    #print('text:')
+                    #print(''.join([x.decode('utf8') for x in d.attrs[f'txt{i}']]))
                     labels.append(''.join([x.decode('utf8') for x in data.attrs[f'txt{i}']]))
                     images.append(k)
             else:
-                print('character bounding boxes (2 x 4 x N):')
+                #print('character bounding boxes (2 x 4 x N):')
                 # print(d.attrs['charBB'])
-                print('text:')
-                print(''.join([x.decode('utf8') for x in data.attrs['txt']]))
+                #print('text:')
+                #print(''.join([x.decode('utf8') for x in data.attrs['txt']]))
                 labels.append(''.join([x.decode('utf8') for x in data.attrs['txt']]))
                 images.append(k)
-            print('label:')
+            #print('label:')
             #print(['train', 'val', 'test'][data.attrs['label']])
 
     num = len(labels)
@@ -48,12 +48,15 @@ def main(input_path, output_path, split_per, data_split):
         valid = dict(zip(valid_l, valid_i))
 
         for tr_k in train.keys():
+            print("train key: {}".format(tr_k))
             with open('{}/train_text.txt'.format(output_path), mode='a') as txt:
                 txt.writelines('{}\t{}\n'.format(f'{train[tr_k]}.png', tr_k))
         for te_k in test.keys():
+            print("test key: {}".format(te_k))
             with open('{}/test_text.txt'.format(output_path), mode='a') as txt:
                 txt.writelines('{}\t{}\n'.format(f'{test[te_k]}.png', te_k))
         for va_k in valid.keys():
+            print("vald key: {}".format(va_k))
             with open('{}/valid_text.txt'.format(output_path), mode='a') as txt:
                 txt.writelines('{}\t{}\n'.format(f'{valid[va_k]}.png', va_k))
 
