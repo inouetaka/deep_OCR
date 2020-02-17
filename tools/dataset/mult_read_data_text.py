d8cc34b246b6575f0fcb278da4d5f22a2f608fe8
inouetaka <webroot63@gmail.com>
2020-02-17 14:06:02 +0900

    print log

--- tools/dataset/mult_read_data_text.py
+++ tools/dataset/mult_read_data_text.py
@@ -6,6 +6,7 @@ import numpy as np
 def main(input_path, output_path, split_per, data_split):
     h5s = os.listdir(input_path.strip("/"))
     labels = []
+    c_labels = []
     images = []
     for h5 in h5s:
         db = h5py.File(f'{input_path.strip("/")}/{h5}')['data']
@@ -19,30 +20,33 @@ def main(input_path, output_path, split_per, data_split):
             if 'count' in data.attrs:
                 count = data.attrs['count']
                 for i in range(count):
-                    #print(f'instance {i}')
-                    #print('character bounding boxes (2 x 4 x N):')
+                    print(f'instance {i}')
+                    print('character bounding boxes (2 x 4 x N):')
                     # print(d.attrs[f'charBB{i}'])
-                    #print('text:')
-                    #print(''.join([x.decode('utf8') for x in d.attrs[f'txt{i}']]))
+                    print('text:')
+                    print(''.join([x.decode('utf8') for x in d.attrs[f'txt{i}']]))
                     labels.append(''.join([x.decode('utf8') for x in data.attrs[f'txt{i}']]))
                     images.append(k)
             else:
-                #print('character bounding boxes (2 x 4 x N):')
+                print('character bounding boxes (2 x 4 x N):')
                 # print(d.attrs['charBB'])
-                #print('text:')
-                #print(''.join([x.decode('utf8') for x in data.attrs['txt']]))
+                print('text:')
+                print(''.join([x.decode('utf8') for x in data.attrs['txt']]))
                 labels.append(''.join([x.decode('utf8') for x in data.attrs['txt']]))
                 images.append(k)
-            #print('label:')
+            print('label:')
             #print(['train', 'val', 'test'][data.attrs['label']])
 
-    num = len(labels)
-    split_per = [float(x.strip()) for x in split_per.split(',')]
-    per = [int(len(labels) * n) for n in [split_per[0], split_per[0] + split_per[1]]]
-    train_l, test_l, valid_l = np.split(labels, per)
-    train_i, test_i, valid_i = np.split(images, per)
+    for l in labels:
+        split_label = l.split("\n")
+        label = "".join(split_label)
+        c_labels.append(label)
 
-    print(f"labels:{labels}")
+    num = len(c_labels)
+    split_per = [float(x.strip()) for x in split_per.split(',')]
+    per = [int(len(c_labels) * n) for n in [split_per[0], split_per[0] + split_per[1]]]
+    train_l, test_l, valid_l = np.split(c_labels, per)
+    train_i, test_i, valid_i = np.split(images, per)
 
     if data_split:
         train = dict(zip(train_l, train_i))
@@ -50,15 +54,12 @@ def main(input_path, output_path, split_per, data_split):
         valid = dict(zip(valid_l, valid_i))
 
         for tr_k in train.keys():
-            print("train key: {}".format(tr_k))
             with open('{}/train_text.txt'.format(output_path), mode='a') as txt:
                 txt.writelines('{}\t{}\n'.format(f'{train[tr_k]}.png', tr_k))
         for te_k in test.keys():
-            print("test key: {}".format(te_k))
             with open('{}/test_text.txt'.format(output_path), mode='a') as txt:
                 txt.writelines('{}\t{}\n'.format(f'{test[te_k]}.png', te_k))
         for va_k in valid.keys():
-            print("vald key: {}".format(va_k))
             with open('{}/valid_text.txt'.format(output_path), mode='a') as txt:
                 txt.writelines('{}\t{}\n'.format(f'{valid[va_k]}.png', va_k))
 
